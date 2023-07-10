615f195c5c4d538415cd01b7ca8a854de0c52da1
freenozero <freenozero@naver.com>
2023-07-10 19:30:07 +0900

    Revert "230710-graph(dfs, bfs)"
    This reverts commit 13fbb28cfad47f5c7ed6dfe50fcb7b6ee7e0b625.

--- BAEKJOON/S/1260.py
+++ BAEKJOON/S/1260.py
@@ -1,35 +1,17 @@
-N, M, V = map(int, input().split())
+import sys
+N, M = map(int, sys.stdin.readline().split())
+site = []
+password = []
+for i in range(N):
+    s, p = sys.stdin.readline().split()
+    site.append(s)
+    password.append(p)
 
-graph = {}
+out = []
 for i in range(M):
-	a, b = map(int, input().split())
-	graph[a] = graph.get(a, []) + [b]
-	graph[b] = graph.get(b, []) + [a]
+    search = sys.stdin.readline().rstrip()
+    idx = site.index(search)
+    out.append(password[idx])
 
-for key in graph:
-    graph[key]  = sorted(graph[key])
-
-
-def BFS(start_v):
-    discovered = [start_v]
-    queue = [start_v]
-    while queue:
-        v = queue.pop(0)
-        if v in graph.keys():
-            for w in graph[v]:
-                if w not in discovered:
-                    discovered.append(w)
-                    queue.append(w)
-    return discovered
-
-
-def DFS(v, discovered=[]):
-    discovered.append(v)
-    if v in graph.keys():
-        for w in graph[v]:
-            if w not in discovered:
-                discovered = DFS(w, discovered)
-    return discovered
-
-print(' '.join(map(str, DFS(V))))
-print(' '.join(map(str, BFS(V))))
+for i in out:
+    print(i)
