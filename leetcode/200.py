13fbb28cfad47f5c7ed6dfe50fcb7b6ee7e0b625
freenozero <freenozero@naver.com>
2023-07-10 19:29:40 +0900

    230710-graph(dfs, bfs)

---
+++ leetcode/200.py
@@ -0,0 +1,15 @@
+def numIslands(grid):
+    """
+    :type grid: List[List[str]]
+    :rtype: int
+    """
+    print(grid)
+
+grid = [
+    ["1","1","1","1","0"],
+    ["1","1","0","1","0"],
+    ["1","1","0","0","0"],
+    ["0","0","0","0","0"]
+    ]
+
+numIslands(grid)
