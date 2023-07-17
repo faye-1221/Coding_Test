67b3c87b10a619cf083f678d0bf3c16f4beefc5f
freenozero <freenozero@naver.com>
2023-07-17 09:01:23 +0900

    230717

---
+++ BAEKJOON/B/27889.py
@@ -0,0 +1,7 @@
+l = {"NLCS" :"North London Collegiate School",
+     "BHA" : "Branksome Hall Asia",
+     "KIS" : "Korea International School",
+     "SJA" : "St. Johnsbury Academy"}
+abb = input()
+
+print(l[abb])
