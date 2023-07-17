bf8bddb055db9bcceff1a9e1b06275dc02296347
freenozero <freenozero@naver.com>
2023-07-17 11:23:47 +0900

    230717

---
+++ BAEKJOON/S/1012.py
@@ -0,0 +1,30 @@
+T = int(input())
+
+def dfs(i, j):
+    if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != 1:
+        return
+    
+    grid[i][j] = 0
+
+    dfs(i + 1, j)
+    dfs(i - 1, j)
+
+    dfs(i, j + 1)
+    dfs(i, j - 1)
+
+for _ in range(T):
+    M, N, K = map(int, input().split())
+    cnt = 0
+    grid = [[0]*M for _ in range(N)]
+
+    for _ in range(K):
+        x, y = map(int, input().split())
+        grid[y][x] = 1
+    
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                dfs(i, j)
+                cnt += 1
+    
+    print(cnt)
