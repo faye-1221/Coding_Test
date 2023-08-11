971ef175cf4d54f3ccd6d92f498ea37c6172f8f6
freenozero <freenozero@naver.com>
2023-08-11 16:04:48 +0900

    230811

---
+++ BAEKJOON/S/11725.py
@@ -0,0 +1,56 @@
+# dfs
+import sys
+sys.setrecursionlimit(10**6)
+n = int(sys.stdin.readline())
+
+graph = [[] for i in range(n+1)]
+
+for i in range(n-1):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [0]*(n+1)
+
+arr = []
+
+def dfs(s):
+    for i in graph[s]:
+        if visited[i] == 0:
+            visited[i] = s
+            dfs(i)
+
+dfs(1)
+
+for x in range(2, n+1):
+    print(visited[x])
+    
+	
+# bfs
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+graph = [[] for i in range(N+1)]
+for _ in range(N-1):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+queue = deque()
+queue.append(1)
+
+ans = [0]*(N+1)
+
+def bfs():
+    while queue:
+        now = queue.popleft()
+        for nxt in graph[now]:
+            if ans[nxt] == 0:
+                ans[nxt] = now
+                queue.append(nxt)
+
+bfs()
+res = ans[2:]
+for x in res:
+    print(x)
---
+++ BAEKJOON/S/2407.py
@@ -0,0 +1,4 @@
+import math
+n, m = map(int, input().split()) 
+
+print(math.comb(n, m)) # 순열: perm()
