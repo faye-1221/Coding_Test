1031ace3cf8de776712506bd915858c4d2660d73
freenozero <freenozero@naver.com>
2023-08-30 13:31:43 +0900

    230830-괄호를 삽입하는 여러가지 방법(분할정복)

---
+++ leetcode/241.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def diffWaysToCompute(self, expression):
+        """
+        :type expression: str
+        :rtype: List[int]
+        """
+        def compute(left, right, op):
+            results = []
+            for l in left:
+                for r in right:
+                    results.append(eval(str(l) + op + str(r)))
+            return results
+        
+        if expression.isdigit():
+            return [int(expression)]
+        
+        results = []
+        for idx, value in enumerate(expression):
+            if value in "-+*":
+                left = self.diffWaysToCompute(expression[:idx])
+                right = self.diffWaysToCompute(expression[idx+1:])
+                results.extend(compute(left, right, value)) 
+        return results   
