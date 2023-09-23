a839c31c0da632728a2bd23ef38420b0696fd641
freenozero <freenozero@naver.com>
2023-09-23 14:03:31 +0900

    230923-네트워크 딜레이 타임

--- leetcode/743.py
+++ leetcode/743.py
@@ -1,13 +1,16 @@
 class Solution(object):
     def networkDelayTime(self, times, n, k):
         graph = collections.defaultdict(list)
-
+        # 그래프 인접 리스트 구성
         for u, v, w in times:
             graph[u].append((v, w))
-
+        
+        # 큐 변수: [(소요 시간, 정점)]
         Q = [(0, k)]
+
         dist = collections.defaultdict(int)
 
+        # 우선순위 큐 최솟값 기준으로 정점까지 최단 경로 삽입
         while Q:
             time, node = heapq.heappop(Q)
             if node not in dist:
@@ -15,8 +18,8 @@ class Solution(object):
                 for v, w in graph[node]:
                     alt = time + w
                     heapq.heappush(Q, (alt, v))
-
+        
+        # 모든 노드의 최단 경로 존재 여부 판별
         if len(dist) == n:
             return max(dist.values())
-        
         return -1
