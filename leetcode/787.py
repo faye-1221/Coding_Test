4587d5e8cf3e84db07439978ae3ca25c2df5a9ca
freenozero <freenozero@naver.com>
2023-09-23 14:29:52 +0900

    230923-K 경유지 내 가장 저렴한 항공권

--- leetcode/787.py
+++ leetcode/787.py
@@ -9,8 +9,6 @@ class Solution(object):
         :rtype: int
         """
         graph = collections.defaultdict(list)
-
-        # 그래프 인접 리스트 구성
         for u, v, w in flights:
             graph[u].append((v, w))
 
@@ -31,3 +29,38 @@ class Solution(object):
                     heapq.heappush(Q, (alt, v, k-1))
         return -1
 
+# 기존 코드에서 추가된 내용
+# https://github.com/onlybooks/python-algorithm-interview/issues/104
+
+# 1. weight
+# - 해당 노드를 방문했던 경로의 (price, k)를 저장
+# 2. 우선순위 큐에 push하는 조건 추가
+# - 해당 노드 weight의 price > push할 노드의 price
+# - 해당 노듸 weight의 k <= push할 노드의 k
+import collections
+import heapq, sys
+from typing import List
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
+        graph = collections.defaultdict(list)
+        weight = [(sys.maxsize, K)] * n
+        # 그래프 인접 리스트 구성
+        for u, v, w in flights:
+            graph[u].append((v, w))
+
+        # 큐 변수: [(가격, 정점, 남은 가능 경유지 수)]
+        Q = [(0, src, K)]
+
+        # 우선 순위 큐 최소값 기준으로 도착점까지 최소 비용 판별
+        while Q:
+            price, node, k = heapq.heappop(Q)
+            if node == dst:
+                return price
+            if k >= 0:
+                for v, w in graph[node]:
+                    alt = price + w
+                    if alt < weight[v][0] or k-1 >= weight[v][1]:
+                        weight[v] = (alt, k-1)
+                        heapq.heappush(Q, (alt, v, k - 1))
+        return -1
