e3de49df8a3e6bfcc530058880bffc77aa880c41
faye-1221 <faye_1221@naver.com>
2024-10-07 12:23:38 +0900

    241007-집합의 표현(다른 예시 추가)

--- BAEKJOON/G/1717.py
+++ BAEKJOON/G/1717.py
@@ -31,3 +31,37 @@ for _ in range(m):
         union(b, c)
     else:
         same_set(b, c)
+        
+'''
+https://developer-project.tistory.com/435
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+li = [i for i in range(n + 1)]
+
+def union(a, b):
+    if (find(a) != find(b)):
+        li[find(b)] = find(a)
+
+def find(a):
+    if (li[a] == a):
+        return a
+    else:
+        li[a] = find(li[a])
+        return li[a]
+    
+def check(a, b):
+    return (find(a) == find(b))
+
+for _ in range(m):
+    op, a, b = map(int, sys.stdin.readline().split())
+
+    if (op == 0):
+        union(a, b)
+    else:
+        if (check(a, b)):
+            print('YES')
+        else:
+            print('NO')
+'''
